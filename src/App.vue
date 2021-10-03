0efbbf68c5d29fc06702a6617b382329b0cb7827
satthabut <satthabut.o@gmail.com>
2021-10-04 03:40:39 +0700

    - [chore] add vue query lib

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,14 @@
   <router-view />
 </template>
 
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useQueryProvider } from "vue-query";
+export default defineComponent({
+  setup() {
+    useQueryProvider();
+  },
+});
+</script>
+
 <style></style>
